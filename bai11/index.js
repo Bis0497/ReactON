a5811459b4b70a6edc83579e1292c9d1ee3a1d7b
Bis0497 <gkhanh.0497@gmail.com>
2023-04-09 18:28:04 +0700

    11 12 js undone

---
+++ bai11/index.js
@@ -0,0 +1,15 @@
+const id = document.getElementById('id');
+const password = document.getElementById('password');
+const loginBtn = document.getElementById('login-btn');
+
+loginBtn.addEventListener('click', function () {
+    console.log('click');
+    // submit()
+})
+
+function submit () {
+  
+    console.log('click')
+    const tdsub = document.createElement('td');
+    const trsub = document.createElement('tr');
+}
